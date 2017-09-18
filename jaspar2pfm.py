f9efb332eda9d97638c26dd2e7de9464ebba4b1a
Oriol Fornés <oriol@cmmt.ubc.ca>
2017-09-18 16:46:24 -0700

    final commit

--- jaspar2pfm.py
+++ jaspar2pfm.py
@@ -49,35 +49,36 @@ if __name__ == "__main__":
     # If JASPAR bundle... #
     if options.jaspar_bundle is not None:
         # Initialize #
-        motifs = []
+        profiles = []
         # For each line... #
         for line in functions.parse_file(os.path.join(options.jaspar_bundle)):
             # If header... #
             if line.startswith(">"):
-                # Add motif #
-                motifs.append([line[1:9]])
+                # Add profile #
+                profiles.append([line])
             # ... Else... #
             else:
                 m = re.search("^\s*\w\s*\[(.+)\]\s*$", line)
-                # Add counts to motif #
-                if m: motifs[-1].append(" ".join([i for i in re.findall("[+-]?[0-9]*[.]?[0-9]+", m.group(1))]))
-        # For each motif... #
-        for motif in motifs:
+                # Add counts to profile #
+                if m: profiles[-1].append(" ".join([i for i in re.findall("[+-]?[0-9]*[.]?[0-9]+", m.group(1))]))
+        # For each profile... #
+        for profile in profiles:
             # Initialize #
-            matrix_id = motif.pop(0)
+            matrix_id = profile[0][1:9]
             # Skip if JASPAR file already exists #
             jaspar_file = os.path.join(os.path.abspath(options.output_dir), "%s.pfm" % matrix_id)
             if not os.path.exists(jaspar_file):
                 # Write #
-                functions.write(jaspar_file, "\n".join(motif))
+                functions.write(jaspar_file, "\n".join(profile))
     # ... Else... #
     else:
-        # For each motif... #
-        for motif in functions.get_jaspar_profiles():
+        # For each profile... #
+        for profile in functions.get_jaspar_profiles():
             # Skip if invalid taxa #
-            if motif.tax_group not in options.tax_groups: continue
+            if profile.tax_group not in options.tax_groups: continue
             # Skip if JASPAR file already exists #
-            jaspar_file = os.path.join(os.path.abspath(options.output_dir), "%s.pfm" % motif.matrix_id)
+            jaspar_file = os.path.join(os.path.abspath(options.output_dir), "%s.pfm" % profile.matrix_id)
             if not os.path.exists(jaspar_file):
                 # Write #
-                functions.write(jaspar_file, motif.format("pfm"))
+                functions.write(jaspar_file, ">%s %s" % (profile.matrix_id, profile.name))
+                functions.write(jaspar_file, profile.format("pfm"))
