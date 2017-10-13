53c040ba0a9e9d607d29fb163494631cadfcf6e1
Oriol Fornés <oriol@cmmt.ubc.ca>
2017-10-13 11:36:10 -0700

    final commit

--- jaspar_search.py
+++ jaspar_search.py
@@ -117,7 +117,7 @@ if __name__ == "__main__":
         # Remove dummy file if exist #
         if os.path.exists(dummy_file): os.remove(dummy_file)
         # Create an empty dummy file #
-        process = subprocess.call(["touch", dummy_file])
+        os.sytem("touch %s" % dummy_file)
         # Skip if output file already exists #
         if os.path.exists(output_file): continue
         # If output file exists in a previous JASPAR release... #
