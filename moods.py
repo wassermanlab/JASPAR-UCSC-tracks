cf8de7e6d1e046951715bf3454225f3f04c89c4b
Oriol Fornés <oriol@cmmt.ubc.ca>
2018-05-23 15:44:43 -0700

    added moods

--- moods.py
+++ moods.py
@@ -60,7 +60,7 @@ if __name__ == "__main__":
     bg = MOODS.tools.flat_bg(4)
     pseudocounts = 0.01
     profile = MOODS.parsers.pfm_to_log_odds(dummy_file, bg, pseudocounts)
-    print(options.pvalue_threshold)
+    print(profile)
     exit(0)
     pvalue_threshold = MOODS.tools.threshold_from_p(profile, bg, options.pvalue_threshold, 4)
     print("here")
