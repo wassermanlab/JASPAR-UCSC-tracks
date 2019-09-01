65da7d30bf265d7d072553549a6a113e05ebda57
Oriol Fornés <oriol@cognac7.cmmt.ubc.ca>
2019-09-01 11:36:55 -0700

    updated genomic tracks tool for JASPAR 2020 release

--- scans2bed.sh
+++ scans2bed.sh
@@ -5,5 +5,5 @@ SCANS_DIR=$2
 
 cut -f 1 $CHROM_SIZES_FILE | LC_COLLATE=C sort -k1,1 -k2,2n |
 {
-while read i; do zgrep "^$i[[:space:]]" $SCANS_DIR/* | cut -d ":" -f 2 | cut -f 1-4,6,7 | sort -k1,1 -k2,2n; done
+while read i; do zless $SCANS_DIR/*.gz | grep "^chrI[[:space:]]" | cut -f 1-4,6,7 | sort -k1,1 -k2,2n; done
 }
