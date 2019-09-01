65eff06203ce98d6131c9d0e89b86c7b390c0324
Oriol Fornés <oriol@gpurtx.cmmt.ubc.ca>
2019-09-01 16:55:29 -0700

    updated bash scripts

--- genomes/ce10/ce10.sh
+++ genomes/ce10/ce10.sh
@@ -1,6 +1,5 @@
 wget http://hgdownload.cse.ucsc.edu/goldenPath/ce10/bigZips/chromFa.tar.gz
 tar xvfz chromFa.tar.gz 
-rm *_*.fa
 cat *.fa > ce10.fa
 rm chromFa.tar.gz
 faidx ce10.fa -i chromsizes > ce10.chrom.sizes
--- genomes/dm6/dm6.sh
+++ genomes/dm6/dm6.sh
@@ -1,5 +1,6 @@
 wget http://hgdownload.soe.ucsc.edu/goldenPath/dm6/bigZips/dm6.fa.gz 
 gunzip dm6.fa.gz 
-cat dm6.fa | perl -e 'while(<>){chomp;if(substr($_,0,1) eq ">"){$chr=substr($_,1);}system("echo \"$_\" >> $chr.fa");}'
-rm *_*.fa
+faidx -x dm6.fa
+rm chr*_*.fa
+cat chr*.fa > dm6.fa
 faidx dm6.fa -i chromsizes > dm6.chrom.sizes
