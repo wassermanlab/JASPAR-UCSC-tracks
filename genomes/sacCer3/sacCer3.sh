5e1259229d0b48b111e41b354084965ad4165bf8
Oriol Fornés <oriol@gpurtx.cmmt.ubc.ca>
2019-09-01 12:50:04 -0700

    updated genomic tracks tool for JASPAR 2020 release

--- genomes/sacCer3/sacCer3.sh
+++ genomes/sacCer3/sacCer3.sh
@@ -1,6 +1,5 @@
 wget http://hgdownload.soe.ucsc.edu/goldenPath/sacCer3/bigZips/chromFa.tar.gz
 tar xvfz chromFa.tar.gz 
-rm chr*_*.fa
 cat chr*.fa > sacCer3.fa
 rm chromFa.tar.gz
 faidx sacCer3.fa -i chromsizes > sacCer3.chrom.sizes
