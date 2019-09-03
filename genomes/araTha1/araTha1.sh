e4b052e899416024fc61908923ab22c71d2ee58a
Oriol Fornés <oriol@cmmt.ubc.ca>
2019-09-03 16:40:16 -0700

    added bash script for araTha1

--- genomes/araTha1/araTha1.sh
+++ genomes/araTha1/araTha1.sh
@@ -1,3 +1,3 @@
 wget https://genome-test.gi.ucsc.edu/~hiram/hubs/Plants/araTha1/araTha1.2bit
-twoBitToFa -udcDir=. araTha1.2bit stdout > araTha1.fa
+twoBitToFa araTha1.2bit araTha1.fa
 faidx araTha1.fa -i chromsizes > araTha1.chrom.sizes
