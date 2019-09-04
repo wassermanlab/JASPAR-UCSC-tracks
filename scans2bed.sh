eb0c5d9d66c497768f1b967b3d11c42c4199cec1
Oriol Fornés <oriol.fornes@gmail.com>
2019-09-04 08:40:36 -0700

    nothing happened

---
+++ scans2bed.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCANS_DIR=$1
+OUTPUT_BED=$2
+
+UNSORTED_BED=.unsorted.bed
+
+zless $SCANS_DIR/*.bed.gz | cut -f 1-4,6,7 > $UNSORTED_BED
+LC_ALL=C sort --parallel=16 --buffer-size=4G -k1,1 -k2,2n $UNSORTED_BED > $OUTPUT_BED
