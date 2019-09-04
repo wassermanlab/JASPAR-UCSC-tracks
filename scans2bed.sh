4306748ef909cd951726d62e0b4bc507483855e4
Oriol Fornés <oriol@cmmt.ubc.ca>
2019-09-03 17:50:37 -0700

    parallelized scans2bed.sh

--- scans2bed.sh
+++ scans2bed.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-CHROM_SIZES_FILE=$1
-SCANS_DIR=$2
+SCANS_DIR=$1
+OUTPUT_BED=$2
 
-cut -f 1 $CHROM_SIZES_FILE | LC_COLLATE=C sort -k1,1 -k2,2n |
-{
-while read i; do zless $SCANS_DIR/*.bed.gz | grep "^$i[[:space:]]" | cut -f 1-4,6,7 | sort -k1,1 -k2,2n | awk -v max=1000 '{if($5>max){print $1"\t"$2"\t"$3"\t"$4"\t"max"\t"$6}else{print $0}}'; done
-}
+UNSORTED_BED=.unsorted.bed
+
+zless $SCANS_DIR/*.bed.gz | cut -f 1-4,6,7 > $UNSORTED_BED
+LC_ALL=C sort --parallel=16 --buffer-size=1G -k1,1 -k2,2n $UNSORTED_BED > $OUTPUT_BED
