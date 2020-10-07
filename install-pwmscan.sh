4018d605fd1388992f06f249d7be136aebd87220
Oriol Fornes <oriol.fornes@gmail.com>
2020-10-07 11:26:32 -0700

    fixed some minor bugs

--- install-pwmscan.sh
+++ install-pwmscan.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+## JASPAR-UCSC-tracks
+## Author: Oriol Fornes
+## Contact: oriol@cmmt.ubc.ca
+
+##
+## Download and install PWMScan
+##
+
 # From https://stackoverflow.com/questions/59895/
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-# Download and install PWMScan 
+mkdir -p $DIR/pwmscan
+cd $DIR/pwmscan
 curl -L -O https://sourceforge.net/projects/pwmscan/files/pwmscan/rel-1.1.9/pwmscan.1.1.9.tar.gz
 tar xvfz pwmscan.1.1.9.tar.gz
-cd $DIR/pwmscan
+cd pwmscan
 mkdir -p bin
 make clean && make cleanbin
 make && make install
