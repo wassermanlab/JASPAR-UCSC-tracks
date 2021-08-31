aab8fb9debc0c8cd3dae9bd68e553191c77e8d76
oriolfornes <oriol.fornes@gmail.com>
2021-08-31 10:45:52 -0700

    added araTha1

--- genomes/get-genomes.sh
+++ genomes/get-genomes.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env bash
 
+if ! [ -f araTha1/araTha1.fa.sizes ]; then
+	mkdir -p araTha1
+	cd araTha1
+	wget https://genome-test.gi.ucsc.edu/~hiram/hubs/Plants/araTha1/araTha1.2bit
+	twoBitToFa araTha1.2bit araTha1.fa
+	faidx -x araTha1.fa
+	faidx araTha1.fa -i chromsizes > araTha1.chrom.sizes
+	rm araTha1.2bit chr*.fa
+	cd ..
+fi
+
 if ! [ -f ce10/ce10.fa.sizes ]; then
 	genomepy install -p UCSC -g ./ -r "^chr[XVIM]{1,3}$" -t 8 -f ce10
 fi
 
 if ! [ -f ce11/ce11.fa.sizes ]; then
 	genomepy install -p UCSC -g ./ -r "^chr[XVIM]{1,3}$" -t 8 -f ce11
+fi
+
+if ! [ -f ci3/ci3.fa.sizes ]; then
+	genomepy install -p UCSC -g ./ -r "^chr[\d]{1,2}$" -t 8 -f ci3
 fi
 
 if ! [ -f danRer11/danRer11.fa.sizes ]; then
